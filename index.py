245d72dabcc84b82bab738fabfb6d47cbe9d8167
Juan <juan.calderon@utec.edu.pe>
2024-02-21 15:11:54 -0500

    noveno commit, ordenando index

--- index.py
+++ index.py
@@ -2,7 +2,9 @@ print("Realizando la tarea")
 
 print("Realizando los pasos de la tarea")
 
+print("Añadiendo los archivos al github")
+
 print("Cambiando a la rama dev")
 
-print("Añadiendo los archivos al github")
+
 
